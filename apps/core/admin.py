6438f5554aebd06033e5edf12530c2b233235594
EMIZY <6UZCF9QtYyR2Y57R5E52>
2022-09-10 07:19:08 +0100

    update:     Register models in their appropriate app django admin

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from apps.core.models import User, Company, Franchise, IceCreamTruck
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "email",
+        "mobile",
+        "date_joined",
+    )
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Company)
+admin.site.register(Franchise)
+admin.site.register(IceCreamTruck)
--- apps/store/admin.py
+++ apps/store/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from apps.store.models import Store, Flavor
 
 # Register your models here.
+admin.site.register(Store)
+admin.site.register(Flavor)
